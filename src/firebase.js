b2d00131c44caa892a41db1000be419e1604bf27
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-04-13 21:41:39 +0000

    Starting Point - cp of firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7a1VONOKXGDq-lOU2-DrDFEzDQ1pSP9M",
+  authDomain: "clone-1168e.firebaseapp.com",
+  projectId: "clone-1168e",
+  storageBucket: "clone-1168e.appspot.com",
+  messagingSenderId: "159323717710",
+  appId: "1:159323717710:web:57379672dd46392193431f",
+  measurementId: "G-CWWTXDS14K",
+};
