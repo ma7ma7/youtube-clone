17e9d0ef79a98b66c1e11219305b7c3d0addebd0
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-04-14 23:24:22 +0000

    :construction: Starting WOrk on Sidebar

---
+++ src/components/RecommendedVideos.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./RecommendedVideos.css";
+function RecommendedVideos() {
+  return <div className="recommendedVideos">Recommended</div>;
+}
+
+export default RecommendedVideos;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return <div className="sidebar">I'm react Sidebar</div>;
+}
+
+export default Sidebar;
