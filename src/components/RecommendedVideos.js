fd070fbdf2b264b27c7fb59c7de295cec11d4ca0
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-04-16 00:50:28 +0000

    :sparkles: Recommended Section UI
    - Build Recommended Section UI
    - Create custom video block component

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -1,7 +1,57 @@
 import React from "react";
+import VideoCard from "./VideoCard";
 import "./RecommendedVideos.css";
+
 function RecommendedVideos() {
-  return <div className="recommendedVideos">Recommended</div>;
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="GitFlow complete Guide 🔥 like Pros"
+          views="1.5M"
+          timestamp="5 mins ago"
+          channelImage="https://images-platform.99static.com//dSQhzTpWXDcpuu9Y2xEpXV84QMg=/129x129:992x992/fit-in/500x500/99designs-contests-attachments/118/118295/attachment_118295052"
+          channel="Codeflix"
+          image="https://i.pinimg.com/originals/c6/bc/01/c6bc016b60029782d51de0f43b6969f1.png"
+        />
+
+        <VideoCard
+          title="GitFlow complete Guide 🔥"
+          views="1.5M"
+          timestamp="5 mins ago"
+          channelImage="https://images-platform.99static.com//dSQhzTpWXDcpuu9Y2xEpXV84QMg=/129x129:992x992/fit-in/500x500/99designs-contests-attachments/118/118295/attachment_118295052"
+          channel="Codeflix"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8cbC9r5ky53YGasTHtNRiOPDl6vqs0EmwTWEqnMT2ZRD54ShXIH6rkU3Z2flLhFu2XSE&usqp=CAU"
+        />
+        <VideoCard
+          title="GitFlow complete Guide 🔥"
+          views="1.5M"
+          timestamp="5 mins ago"
+          channelImage="https://images-platform.99static.com//dSQhzTpWXDcpuu9Y2xEpXV84QMg=/129x129:992x992/fit-in/500x500/99designs-contests-attachments/118/118295/attachment_118295052"
+          channel="Codeflix"
+          image="https://d2slcw3kip6qmk.cloudfront.net/marketing/blogs/tech/pkg-manager-blog-post-header@2x.png"
+        />
+
+        <VideoCard
+          title="GitFlow complete Guide 🔥"
+          views="1.5M"
+          timestamp="5 mins ago"
+          channelImage="https://images-platform.99static.com//dSQhzTpWXDcpuu9Y2xEpXV84QMg=/129x129:992x992/fit-in/500x500/99designs-contests-attachments/118/118295/attachment_118295052"
+          channel="Codeflix"
+          image="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/141529629/original/2c92a37b1cfb8a96739f5166c55ac43665af4041/design-amazing-youtube-thumbnail-in-1-hour.jpg"
+        />
+        <VideoCard
+          title="GitFlow complete Guide 🔥"
+          views="1.5M"
+          timestamp="5 mins ago"
+          channelImage="https://images-platform.99static.com//dSQhzTpWXDcpuu9Y2xEpXV84QMg=/129x129:992x992/fit-in/500x500/99designs-contests-attachments/118/118295/attachment_118295052"
+          channel="Codeflix"
+          image="https://i.pinimg.com/originals/e2/0c/b7/e20cb7604bf8a773db901bacc10d6e45.jpg"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default RecommendedVideos;
