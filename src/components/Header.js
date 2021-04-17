70dd8bd5979f6c88e9dac782d0e403b6cf9c364a
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-04-17 01:21:40 +0000

    :art: Start Creating Search Component
    - Implement React Router
    - Change Route on search
    - Starting SearchPage Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
-
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const [inputSearch, setinputSearch] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <img
-          className="header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1280px-Logo_of_YouTube_%282015-2017%29.svg.png"
-          alt=""
-        />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1280px-Logo_of_YouTube_%282015-2017%29.svg.png"
+            alt=""
+          />
+        </Link>
       </div>
 
       <div className="header__input">
-        <input placeholder="Search" type="text" />
-        <SearchIcon className="header__inputButton" />
+        <input
+          onChange={(e) => setinputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
 
       <div className="header__icons">
